92e1b80291bf6abf9fdef66d5c1c983bcee0863d
Afaq Zahir <afaqxdev@gmail.com>
2024-04-02 16:52:46 -0700

    add

---
+++ android/app/src/main/kotlin/com/example/drop_down/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.drop_down
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/dropdown.dart
@@ -0,0 +1,173 @@
+import 'package:daaem_audit/Core/Constant/Colors/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+// ignore: must_be_immutable
+class CustomDropdownButton extends StatefulWidget {
+  final String hint;
+
+  final List<String> items;
+  final String? dropvalue;
+  final ValueChanged<String?>? onChanged;
+  Color? backgroundColor;
+  Color? color;
+  double? size;
+  final double iconsizes;
+  FontWeight? fontWeight;
+  final double? width;
+  final double? height;
+
+  CustomDropdownButton({
+    Key? key,
+    required this.hint,
+    this.color,
+    required this.items,
+    required this.onChanged,
+    this.dropvalue,
+    required this.iconsizes,
+    this.backgroundColor = white,
+    this.size = 16,
+    this.width,
+    this.fontWeight = FontWeight.w600,
+    this.height,
+  }) : super(key: key);
+
+  @override
+  State<CustomDropdownButton> createState() => _CustomDropdownButtonState();
+}
+
+class _CustomDropdownButtonState extends State<CustomDropdownButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: widget.width,
+      height: widget.height,
+      padding: const EdgeInsets.only(left: 15, top: 10, bottom: 10, right: 15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: widget.backgroundColor,
+      ),
+      child: Center(
+        child: DropdownButtonHideUnderline(
+          child: DropdownButton<String>(
+            menuMaxHeight: 200,
+            value: widget.dropvalue,
+            focusColor: widget.backgroundColor,
+            dropdownColor: white,
+            iconEnabledColor: widget.color,
+            iconDisabledColor: widget.color,
+            isExpanded: true,
+            isDense: true,
+            borderRadius: BorderRadius.circular(10.r),
+            style: TextStyle(
+                color: black,
+                fontSize: 16.sp,
+                fontWeight: FontWeight.w400,
+                fontFamily: 'Montserrat'),
+            iconSize: widget.iconsizes,
+            hint: Text(
+              widget.hint,
+              style: TextStyle(
+                  color: widget.color,
+                  fontSize: 14.sp,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: 'Montserrat'),
+            ),
+            onChanged: widget.onChanged,
+            items: widget.items
+                .toSet()
+                .toList()
+                .map<DropdownMenuItem<String>>((value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ModelDropDown<T> extends StatefulWidget {
+  const ModelDropDown({
+    super.key,
+    required this.hint,
+    this.color,
+    required this.items,
+    required this.onChanged,
+    required this.iconsizes,
+    this.backgroundColor = white,
+    this.size = 16,
+    this.width,
+    this.fontWeight = FontWeight.w600,
+    this.onTap,
+    this.height,
+    this.selectedValue,
+  });
+
+  final String hint;
+  final T? selectedValue;
+  final void Function()? onTap;
+  final List<DropdownMenuItem<T>> items;
+  final ValueChanged<T?>? onChanged;
+  final Color? backgroundColor;
+  final Color? color;
+  final double? size;
+  final double iconsizes;
+  final FontWeight? fontWeight;
+  final double? width;
+  final double? height;
+
+  @override
+  State<ModelDropDown<T>> createState() => _ModelDropDownState<T>();
+}
+
+class _ModelDropDownState<T> extends State<ModelDropDown<T>> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: widget.width,
+      height: widget.height,
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12.r),
+          color: widget.backgroundColor,
+          boxShadow: const [
+            BoxShadow(color: grey, blurStyle: BlurStyle.outer, blurRadius: 1)
+          ]),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton<T>(
+          menuMaxHeight: 200,
+          value: widget.selectedValue,
+          focusColor: widget.backgroundColor,
+          dropdownColor: white,
+          iconEnabledColor: widget.color,
+          iconDisabledColor: widget.color,
+          isExpanded: true,
+          isDense: true,
+          borderRadius: BorderRadius.circular(10),
+          style: const TextStyle(
+            color: black,
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            fontFamily: 'Montserrat',
+          ),
+          iconSize: widget.iconsizes,
+          hint: Text(
+            widget.hint,
+            style: TextStyle(
+              color: widget.color,
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+              fontFamily: 'Montserrat',
+            ),
+          ),
+          onChanged: widget.onChanged,
+          items: widget.items,
+        ),
+      ),
+    );
+  }
+}
