273451f191ac331eac78ad959788e5a5149ce804
Afaq Zahir <afaqxdev@gmail.com>
2024-04-09 23:42:22 -0700

    add

---
+++ lib/api/code/home.dart
@@ -0,0 +1,117 @@
+import 'package:drop_down/api/product_model.dart';
+import 'package:drop_down/api/provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class MyHomePagess extends StatefulWidget {
+  const MyHomePagess({
+    super.key,
+  });
+
+  @override
+  State<MyHomePagess> createState() => _MyHomePagessState();
+}
+
+class _MyHomePagessState extends State<MyHomePagess> {
+  ProductModel? selectedCategory;
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<PostState>(context);
+    provider.list.map((ProductModel categoryModel) {
+      return DropdownMenuItem<ProductModel>(
+        onTap: () {},
+        value: categoryModel,
+        child: Text(categoryModel.title
+            .toString()), // Assuming RetailerModel has a 'name' property
+      );
+    }).toList();
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+      ),
+      body: Consumer<PostState>(builder: (context, value, child) {
+        if (value.postStatus == PostStatus.success) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // ModelDropDown<ProductModel>(
+                //   height: 55.h,
+                //   hint: 'Select a Category',
+                //   selectedValue: selectedCategory,
+                //   iconsizes: 24,
+                //   items: category,
+                //   onChanged: (value) async {
+                //     selectedCategory = value;
+                //     setState(() {});
+                //   },
+                // ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: value.list.length,
+                    itemBuilder: (context, index) {
+                      return Image.network(value.list[index].image.toString());
+                    },
+                  ),
+                )
+              ],
+            ),
+          );
+        } else if (value.postStatus == PostStatus.error) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  value.message, // Display the error message
+                  style: const TextStyle(color: Colors.red),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    provider.fetchData(); // Retry fetching data
+                  },
+                  child: const Text('Retry'),
+                ),
+              ],
+            ),
+          );
+        } else {
+          // Handle success state here
+          // For example, display fetched data
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  value.message, // Display the error message
+                  style: const TextStyle(color: Colors.red),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    value.fetchData(); // Retry fetching data
+                  },
+                  child: const Text('Retry'),
+                ),
+              ],
+            ),
+          );
+        }
+      }),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          provider.fetchData();
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/api/code/provider.dart
@@ -0,0 +1,113 @@
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
+
+import 'package:drop_down/api/product_model.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as https;
+import 'package:provider/provider.dart';
+
+enum PostStatus { loading, success, error }
+
+class PostState extends ChangeNotifier {
+  PostStatus _postStatus = PostStatus.loading;
+  List<ProductModel> _list = [];
+  String _message = '';
+  String _searchMessage = '';
+
+  // Getters for accessing state properties
+  PostStatus get postStatus => _postStatus;
+  List<ProductModel> get list => _list;
+  String get message => _message;
+  String get searchMessage => _searchMessage;
+
+  // Method for updating state
+  void updateState({
+    PostStatus? postStatus,
+    List<ProductModel>? list,
+    List<ProductModel>? searchList,
+    String? message,
+    String? searchMessage,
+  }) {
+    _list = list ?? _list;
+    _message = message ?? _message;
+    _searchMessage = searchMessage ?? _searchMessage;
+    _postStatus = postStatus ?? _postStatus;
+    notifyListeners(); // Notify listeners about the state change
+  }
+
+  // Method for making API call
+  Future<void> fetchData() async {
+    final List<ProductModel> apiData =
+        []; // Replace this with your actual API response
+    try {
+      // Simulate API call
+      final Uri uri = Uri.parse("https://fakestoreapi.com/products");
+      final response = await https.get(uri);
+      if (response.statusCode == 200) {
+        final jsonDecode = json.decode(response.body.toString());
+        for (var i in jsonDecode) {
+          print(i);
+          apiData.add(ProductModel.fromJson(i));
+        }
+      }
+
+      updateState(
+        postStatus: PostStatus.success,
+        list: apiData,
+      );
+    } catch (e) {
+      updateState(postStatus: PostStatus.error, message: e.toString());
+    }
+  }
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => PostState(), // Provide an instance of PostState
+      child: const MaterialApp(
+        title: 'Flutter Demo',
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final postState =
+        Provider.of<PostState>(context); // Access the PostState instance
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Provider Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Post Status: ${postState.postStatus}'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                postState.fetchData(); // Call the fetchData method
+              },
+              child: const Text('Fetch Data'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
